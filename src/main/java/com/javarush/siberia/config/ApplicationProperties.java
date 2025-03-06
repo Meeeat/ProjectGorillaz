c6eb280effc686ad73282509f4c014c86c6d5f2e
Siberia <angroov@gmail.com>
2025-03-06 17:28:05 +0900

    ADD config prepare

---
+++ src/main/java/com/javarush/siberia/config/ApplicationProperties.java
@@ -0,0 +1,58 @@
+package com.javarush.siberia.config;
+
+import lombok.SneakyThrows;
+
+import java.io.FileReader;
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Properties;
+
+public class ApplicationProperties extends Properties {
+
+    public static final String HIBERNATE_CONNECTION_URL = "hibernate.connection.url";
+    public static final String HIBERNATE_CONNECTION_USERNAME = "hibernate.connection.username";
+    public static final String HIBERNATE_CONNECTION_PASSWORD = "hibernate.connection.password";
+    public static final String HIBERNATE_CONNECTION_DRIVER_CLASS = "hibernate.connection.driver_class";
+
+    @SneakyThrows
+    public ApplicationProperties() {
+        this.load(new FileReader(CLASSES_ROOT + "/application.properties"));
+        injectEnvironmentVariables();
+        try {
+            String driver = this.getProperty(HIBERNATE_CONNECTION_DRIVER_CLASS);
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void injectEnvironmentVariables() {
+        this.forEach((key, value) -> {
+            String strKey = key.toString();
+            String strValue = value.toString();
+            if (strValue.startsWith("${") && strValue.endsWith("}")) {
+                String[] valueParts = strValue
+                        .replace("${","")
+                        .replace("}","")
+                        .split(":", 2);
+                if (valueParts.length == 2) {
+                    strValue = valueParts[1];
+                }
+                String keyEnvironmentVariable = valueParts[0];
+                String valueEnvironmentVariable = System.getProperties().getProperty(keyEnvironmentVariable);
+                if (keyEnvironmentVariable != null && valueEnvironmentVariable != null) {
+                    strValue = valueEnvironmentVariable;
+                }
+                this.setProperty(strKey, strValue);
+            }
+        });
+    }
+
+    public final static Path CLASSES_ROOT = Paths.get(URI.create(
+            Objects.requireNonNull(
+                    ApplicationProperties.class.getResource("/")
+            ).toString()));
+
+}
